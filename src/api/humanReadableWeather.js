9dbc8e4e804d13c9b803ec2e47853cdb5e8642ed
serhiikovalenko <kovalenkoserg1990@gmail.com>
2025-05-13 22:01:28 +0300

    rm some

--- src/api/humanReadableWeather.js
+++ src/api/humanReadableWeather.js
@@ -1,5 +1,3 @@
-//https://api.openweathermap.org/data/3.0/onecall/overview?lat={lat}&lon={lon}&appid={API key}
-
 export const humanReadableWeather = async (lat, lon, key) => {
   const response = await fetch(
     `https://api.openweathermap.org/data/3.0/onecall/overview?lat=${lat}&lon=${lon}&appid=${key}&units=metric`,
