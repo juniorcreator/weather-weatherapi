73584489eed3b91daa6a8b85af38adbe906fd4ed
serhiikovalenko <kovalenkoserg1990@gmail.com>
2025-05-13 22:03:03 +0300

    rm some -

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,11 @@ import Search from '@/components/Search.vue';
 import Layout from '@/components/UI/Layout.vue';
 import Spinner from '@/components/Spinner.vue';
 import { useWeatherStore } from '@/stores/weather.js';
-import { formatDate } from '@/utils/formatDate.js';
 const { state } = useWeatherStore();
 </script>
 
 <template>
   <Layout>
-    <div class="text-white">
-      <button @click="formatDate(1747155600, ' one')">One</button>
-      <br />
-      <button @click="formatDate(1747159200, ' two')">two</button>
-    </div>
     <Logo />
     <Search />
     <Spinner v-if="state.loading" />
